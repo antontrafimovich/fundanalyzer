2331b7bf6308fdb7a34bb8f2e08989a3c6f12958
Anton Trafimovich <tonytrof@gmail.com>
2024-11-08 11:35:36 +0100

    Turned reader into http server.

--- packages/reader/index.mjs
+++ packages/reader/index.mjs
@@ -1,7 +1,8 @@
 import { parse } from "node-html-parser";
-import fs from "node:fs/promises";
-
+import { createServer } from "http";
 import process from "node:process";
+import { createReadStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
 
 const domain = process.env.DOMAIN;
 
@@ -11,7 +12,7 @@ const cashflowReportPageRoute = process.env.CASHFLOW_REPORT_PAGE_ROUTE;
 
 const companyRoute = process.env.COMPANY_ROUTE;
 
-const url = `${domain}/${balanceReportPageRoute}/${companyRoute}`;
+const url = `${domain}/${profitAndLossPageRoute}/${companyRoute}`;
 
 async function fetchAndParseHTML(url) {
   const response = await fetch(url);
@@ -29,34 +30,107 @@ async function fetchAndParseHTML(url) {
 }
 
 // Example usage
-fetchAndParseHTML(url).then((document) => {
+// fetchAndParseHTML(url).then((document) => {
+//   const years = document
+//     .querySelectorAll(".report-table th")
+//     .map((i) => i.firstChild.textContent.trim())
+//     .filter(Boolean);
+
+//   const metricRows = document.querySelectorAll(".report-table tr").slice(1);
+
+//   let fiancialDataPerYear = years.map((year) => ({ year }));
+
+//   for (let i = 0; i < metricRows.length; i++) {
+//     const metricRow = metricRows[i];
+//     const metricName = metricRow.querySelector("td.f").innerText.trim();
+
+//     const metricValuesCells = metricRow.querySelectorAll("td").slice(1, -1);
+
+//     fiancialDataPerYear.forEach((data, index) => {
+//       const metricValue = metricValuesCells[index].querySelector(".value");
+
+//       if (!metricValue) {
+//         data[metricName] = "";
+//         return;
+//       }
+
+//       data[metricName] = metricValue.textContent.trim();
+//     });
+//   }
+
+//   console.log(fiancialDataPerYear);
+//   // Do something with the document
+// });
+
+const getData = async (url) => {
+  const document = await fetchAndParseHTML(url);
+
   const years = document
     .querySelectorAll(".report-table th")
     .map((i) => i.firstChild.textContent.trim())
     .filter(Boolean);
 
-  console.log("years", years);
+  const metricRows = document.querySelectorAll(".report-table tr").slice(1);
 
-  const rows = document.querySelectorAll(".report-table tr").slice(1);
+  let fiancialDataPerYear = years.map((year) => ({ year }));
 
-  let eachYearFiancialData = years.map((year) => ({ year }));
-  for (let i = 0; i < rows.length; i++) {
-    const row = rows[i];
-    const name = row.querySelector("td.f").innerText.trim();
+  for (let i = 0; i < metricRows.length; i++) {
+    const metricRow = metricRows[i];
+    const metricName = metricRow.querySelector("td.f").innerText.trim();
 
-    console.log("name is ", name);
+    const metricValuesCells = metricRow.querySelectorAll("td").slice(1, -1);
 
-    const cells = row.querySelectorAll("td").slice(1, -1);
+    fiancialDataPerYear.forEach((data, index) => {
+      const metricValue = metricValuesCells[index].querySelector(".value");
 
-    console.log(cells.length);
+      if (!metricValue) {
+        data[metricName] = "";
+        return;
+      }
 
-    eachYearFiancialData.forEach((r, index) => {
-      // console.log(cells[index].querySelector(".value"))
-      console.log(eachYearFiancialData)
-      r[name] = cells[index].querySelector(".value").textContent.trim();
+      data[metricName] = metricValue.textContent.trim();
     });
   }
 
-  console.log(eachYearFiancialData);
-  // Do something with the document
+  return fiancialDataPerYear;
+};
+
+const server = createServer(async (req, res) => {
+  const urlParts = req.url.split("/");
+  const segment = urlParts[1];
+
+  if (req.method === "GET" && segment === "companies") {
+    const r = createReadStream("./tickers.json");
+    console.log("Request for companies");
+    try {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      await pipeline(r, res);
+    } catch (error) {
+      console.error(error);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    }
+
+    return;
+  }
+
+  if (req.method === "GET" && segment) {
+    const fetchUrl = `${domain}/${profitAndLossPageRoute}/${segment}`;
+    try {
+      const data = await getData(fetchUrl);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(data));
+    } catch (error) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    }
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
