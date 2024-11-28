acd66b13d64fcb734fbfda9e927ce6f68c834e01
Anton Trafimovich <tonytrof@gmail.com>
2024-11-28 12:50:25 +0100

    Added ability to read shares info.

--- packages/reader/index.mjs
+++ packages/reader/index.mjs
@@ -10,55 +10,15 @@ const profitAndLossPageRoute = process.env.PROFIT_AND_LOSS_PAGE_ROUTE;
 const balanceReportPageRoute = process.env.BALANCE_REPORT_PAGE_ROUTE;
 const cashflowReportPageRoute = process.env.CASHFLOW_REPORT_PAGE_ROUTE;
 
-const companyRoute = process.env.COMPANY_ROUTE;
-
-const url = `${domain}/${profitAndLossPageRoute}/${companyRoute}`;
+const ratiosPageRoute = process.env.RATIOS_PAGE_ROUTE;
 
 async function fetchAndParseHTML(url) {
   const response = await fetch(url);
-
   const data = await response.text();
-  // const data = await fs.readFile("./data.html", "utf-8");
-
-  // fs.writeFile("./data.html", data);
-
   const dom = parse(data);
 
   return dom;
 }
-
-// Example usage
-// fetchAndParseHTML(url).then((document) => {
-//   const years = document
-//     .querySelectorAll(".report-table th")
-//     .map((i) => i.firstChild.textContent.trim())
-//     .filter(Boolean);
-
-//   const metricRows = document.querySelectorAll(".report-table tr").slice(1);
-
-//   let fiancialDataPerYear = years.map((year) => ({ year }));
-
-//   for (let i = 0; i < metricRows.length; i++) {
-//     const metricRow = metricRows[i];
-//     const metricName = metricRow.querySelector("td.f").innerText.trim();
-
-//     const metricValuesCells = metricRow.querySelectorAll("td").slice(1, -1);
-
-//     fiancialDataPerYear.forEach((data, index) => {
-//       const metricValue = metricValuesCells[index].querySelector(".value");
-
-//       if (!metricValue) {
-//         data[metricName] = "";
-//         return;
-//       }
-
-//       data[metricName] = metricValue.textContent.trim();
-//     });
-//   }
-
-//   console.log(fiancialDataPerYear);
-//   // Do something with the document
-// });
 
 const getData = async (url) => {
   const document = await fetchAndParseHTML(url);
@@ -95,7 +55,7 @@ const getData = async (url) => {
 
 const server = createServer(async (req, res) => {
   const urlParts = req.url.split("/");
-  const segment = urlParts[1];
+  const [, segment, payload] = urlParts;
 
   if (req.method === "GET" && segment === "companies") {
     const r = createReadStream("./tickers.json");
@@ -105,6 +65,21 @@ const server = createServer(async (req, res) => {
       await pipeline(r, res);
     } catch (error) {
       console.error(error);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    }
+
+    return;
+  }
+
+  if (req.method === "GET" && segment === "shares" && payload) {
+    const fetchUrl = `${domain}/${ratiosPageRoute}/${payload}`;
+
+    try {
+      const data = await getData(fetchUrl);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(data));
+    } catch (error) {
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Internal Server Error");
     }
