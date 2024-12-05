fbf647e7bbc58b1c529da01059b853a6cde8132d
Anton Trafimovich <tonytrof@gmail.com>
2024-12-05 12:06:24 +0100

    Added basic charts component.

--- packages/next-app/src/app/details/[tickerId]/revenue-chart/revenue-chart.tsx
+++ packages/next-app/src/app/details/[tickerId]/revenue-chart/revenue-chart.tsx
@@ -3,6 +3,14 @@ import { getSharesInfo } from "./api/ticker-shares.api";
 
 export default async function RevenueChart({ tickerId }: { tickerId: string }) {
   const data = await getTickerInfo(tickerId);
+
+  const chartData = data!.map((row) => {
+    return {
+      year: row.year,
+      revenue: row["Przychody ze sprzedaży"] / row["Liczba akcji"],
+      profit: row["Zysk (strata) brutto"],
+    };
+  });
   // const shares = await getSharesInfo(tickerId);
 
   return <div>In progress</div>;
