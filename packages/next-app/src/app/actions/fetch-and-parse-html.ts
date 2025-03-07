2b7cdc2ae0f994df77aad4c3c6042e5f784803c4
Anton Trafimovich <tonytrof@gmail.com>
2025-03-07 11:53:30 +0100

    Added caching functionality to fetch nd parse.

--- packages/next-app/src/app/actions/fetch-and-parse-html.ts
+++ packages/next-app/src/app/actions/fetch-and-parse-html.ts
@@ -1,7 +1,7 @@
 import { parse } from "node-html-parser";
 
 export async function fetchAndParseHTML(url: string) {
-  const response = await fetch(url);
+  const response = await fetch(url, { next: { revalidate: 3600 } });
   const data = await response.text();
   const dom = parse(data);
 
